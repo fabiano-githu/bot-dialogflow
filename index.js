27745f0eb725be249ecf94b35a593d550ecd6e3d
Fabiano Chavier <chaviersantos70@gmail.com>
2026-06-28 22:05:44 -0300

    Projeto
    ok

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  const quantidade = req.body.queryResult.parameters.quantidade;
+  const preco = req.body.queryResult.parameters.preco;
+
+  const total = quantidade * preco;
+
+  res.json({
+    fulfillmentText: `🧾 Total do pedido: R$ ${total}`
+  });
+});
+
+// Render usa PORT automático
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
